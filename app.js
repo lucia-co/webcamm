2a518c886d632add1f67200c3a84c3b9e754d0e4
lucia-co <64637016+lucia-co@users.noreply.github.com>
2020-05-12 21:47:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ function cameraStart() {
     });
 }
 // Take a picture when cameraTrigger is tapped
-//cameraTrigger.onclick = function() {
-   /// cameraSensor.width = cameraView.videoWidth;
-   // cameraSensor.height = cameraView.videoHeight;
-   // cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-  //  cameraOutput.src = cameraSensor.toDataURL("image/webp");
-  //  cameraOutput.classList.add("taken");
-//};
+cameraTrigger.onclick = function() {
+ cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+   cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+   cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
